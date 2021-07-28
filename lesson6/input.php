e69e47be2c313e615e4e825d49d6142736e3773a
200202iqbal <75594145+200202iqbal@users.noreply.github.com>
2021-07-28 12:43:09 +0900

    sessions

--- lesson6/input.php
+++ lesson6/input.php
@@ -4,13 +4,78 @@ define("CSV_PATH","data/users.csv");
 if (!empty($_POST))
 {
     //POSTがあれば書き込み
-    insert($_POST);
+    
+    $errors=insert($_POST);
 }
+//ユーザ読み込み
+$users=load();
 //データを書き込む
 function insert($posts)
 {
     initCSV();
+    //データチェック
+    $errors = validate($posts);
+    if($errors)
+    {
+        //エラーがあったら返す
+        return $errors;
+    }
+    //パスワードの暗号化
+    $posts["password"] = password_hash($posts["password"],PASSWORD_BCRYPT);
+    //ファイルを開く(add)
+    $file = fopen(CSV_PATH,"a");
+    //データの追加を書き込み
+    fputcsv($file,$posts);
+    //ファイルを閉じる
+    fclose($file);
 }
+
+function validate($data)
+{
+    $errors = [];
+    if(empty($data["name"]))
+    {
+        $errors["name"] = "氏名が入力されていません";
+    }
+    if(empty($data["email"]))
+    {
+        $errors["email"] = "Emailが入力されていません";
+    }
+    if(empty($data["password"]))
+    {
+        $errors["password"] = "パスワードが入力されていません";
+    }
+    return $errors;
+}
+
+function load()
+{
+    $items =[];
+    if(file_exists(CSV_PATH))
+    {
+        //ファイルがあればファイルを開く(read only)
+        $file=fopen(CSV_PATH,"r");
+        //カラムを読み込む
+        $columns = fgetcsv($file);
+        //var_dump($column);
+        //var_dump($columns);
+        //1行ずつCSV読み込み
+        while($data = fgetcsv($file))
+        {
+            //カラムを繰り返す
+            foreach($columns as $index => $column)
+            {
+                $item[$column] = $data[$index];
+            }
+                //商品を追加
+                $items[] = $item;
+            }
+            fclose($file);
+        }
+    return $items;
+}
+
+
 //CSVの初期化
 function initCSV()
 {
@@ -21,6 +86,7 @@ function initCSV()
     {
         //もしデータがなければ
         $data = ["name","email","password"];
+        //ファイルに書き込み(write)
         fputcsv($file,$data);
     }
     fclose(($file));
@@ -45,6 +111,7 @@ function initCSV()
             <div class="itemContainer">
                 <h2>氏名</h2>
                 <input type="text" name="name">
+                <p></p>
             </div>
            <div class="itemContainer">
                 <h2>Email</h2>
@@ -60,5 +127,16 @@ function initCSV()
             
         </form>
     </div>
+    <h2>ユーザ</h2>
+    <table>
+        <?php if($users): ?>
+        <?php foreach($users as $user): ?>
+            <tr>
+                <td><?= $user["name"]?></td>
+                <td><?= $user["email"]?></td>
+            </tr>
+        <?php endforeach?>
+        <?php endif ?>
+    </table>
 </body>
 </html>
